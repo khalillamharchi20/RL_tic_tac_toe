007217684b5b9239793edb9e919fdf4ceeeeb5d3
khalillamharchi20 <khalillamharchi20@gmail.com>
2025-12-30 15:38:12 +0100

    fixing environement

--- env.py
+++ env.py
@@ -1,67 +1,75 @@
-import numpy as np
 import random
-from collections import defaultdict
-import matplotlib.pyplot as plt
+import numpy as np
 
 class TicTacToeEnv:
-    def __init__(self):
+    def __init__(self, opponent="random"):
+        self.opponent = opponent
         self.reset()
-    
+
     def reset(self):
-        # State as a list of 9 numbers: 0=empty, 1=player, -1=opponent
         self.state = [0] * 9
-        self.current_player = 1  
         self.done = False
         self.winner = 0
-        return self.state.copy()
-    
+        return self._obs()
+
+    def _obs(self):
+        # observation is always from the learning agent view: agent uses +1
+        return np.array(self.state, dtype=np.float32)
+
     def get_available_moves(self, state=None):
         if state is None:
             state = self.state
         return [i for i in range(9) if state[i] == 0]
-    
-    def step(self, action):
 
-        if self.state[action] != 0:
-            return self.state, -10, True, {}  
-        
-
-        self.state[action] = self.current_player
-        
-
-        self.done, self.winner = self.check_game_over()
-        
-
-        if self.done:
-            if self.winner == 1:
-                reward = 1
-            elif self.winner == -1:
-                reward = -1
-            else:
-                reward = 0
-        else:
-            reward = 0
-            self.current_player *= -1
-        
-        return self.state.copy(), reward, self.done, {}
-    
     def check_game_over(self):
-        state = self.state
-        
+        s = self.state
         lines = [
-            [0, 1, 2], [3, 4, 5], [6, 7, 8],  
-            [0, 3, 6], [1, 4, 7], [2, 5, 8],  
-            [0, 4, 8], [2, 4, 6]              
+            [0,1,2],[3,4,5],[6,7,8],
+            [0,3,6],[1,4,7],[2,5,8],
+            [0,4,8],[2,4,6]
         ]
-        
-        for line in lines:
-            if state[line[0]] == state[line[1]] == state[line[2]] != 0:
-                return True, state[line[0]]
-        
-        if 0 not in state:
+        for a,b,c in lines:
+            if s[a] == s[b] == s[c] != 0:
+                return True, s[a]
+        if 0 not in s:
             return True, 0
-        
         return False, 0
+
+    def _opponent_move(self):
+        moves = self.get_available_moves()
+        if not moves:
+            return
+        if self.opponent == "random":
+            a = random.choice(moves)
+        else:
+            a = random.choice(moves)
+        self.state[a] = -1  # opponent is -1
+
+    def step(self, action):
+        if self.done:
+            return self._obs(), 0.0, True, {}
+
+        # illegal move
+        if self.state[action] != 0:
+            self.done = True
+            self.winner = -1
+            return self._obs(), -1.0, True, {"illegal": True}
+
+        # agent plays +1
+        self.state[action] = 1
+        self.done, self.winner = self.check_game_over()
+        if self.done:
+            r = 1.0 if self.winner == 1 else 0.0
+            return self._obs(), r, True, {}
+
+        # opponent plays -1
+        self._opponent_move()
+        self.done, self.winner = self.check_game_over()
+        if self.done:
+            r = -1.0 if self.winner == -1 else 0.0
+            return self._obs(), r, True, {}
+
+        return self._obs(), 0.0, False, {}
     
     def render(self):
         symbols = {1: 'X', -1: 'O', 0: ' '}
